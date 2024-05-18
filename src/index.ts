56dd1070a639920722fa371dc6116f2d8de46093
RAVINTEJA <ravinteja.chilukamari@gmail.com>
2024-05-18 20:42:38 +0530

    refactor: Update Express server routes to use "/api" prefix

--- src/index.ts
+++ src/index.ts
@@ -7,12 +7,12 @@ dotenv.config();
 const app: Express = express();
 const port = process.env.PORT || 3000;
 
-app.get("/", (req: Request, res: Response) => {
+app.get("/api", (req: Request, res: Response) => {
   res.send("Express + TypeScript Server");
 });
 
-app.use("/users", usersRoutes);
+app.use("/api/users", usersRoutes);
 
 app.listen(port, () => {
-  console.log(`[server]: Server is running at http://localhost:${port}`);
+  console.log(`[server]: Server is running at http://localhost:${port}/api`);
 });
